2f9fe47146554636e8a16b3e031d775e8d055224
Anirban <banerj10@illinois.edu>
2018-07-16 22:58:42 +0700

    commiting current progress

--- README.txt
+++ README.txt
@@ -1,4 +1,22 @@
-RESTful API for an address book which uses an Elasticsearch data store.
+RESTful API for an address book, implemented in Python using Flask.
 
-This project uses a virtualenv named adbook.
-A list of modules needed for running this project can be found in the requirements.txt file. 
+A list of dependencies can be found in the requirements.txt file.
+
+DATA:
+
+The contact data in the address book as three fields: name, address, and phone number. Of these three, name is used as a unique identifier (i.e. no two entries have the same name).
+Currently, the contact data is sourced from the hard-coded 'contacts' list in adbook.py. In the full implementation, the contact data would be stored on an Elasticsearch data store instead, and all references to the list would be replaced with Elasticsearch calls. 
+
+ENDPOINTS:
+
+http://127.0.0.1:5000
+This serves as a home page. In the current implementation, it does not have any significant functionality and merely provides a title page.
+
+http://127.0.0.1:5000/contact?pageSize={}&page={}&query={}
+This serves GET requests. It provides a listing of the contact data. THe user selects how many entries are to be shown with pageSize, and chooses the offset using page (starting at 0). If the pageSize/page selection is completely outside the list bounds, an error message is returned. However, if it is only partially outsie the list bounds, all entries until the end of the list are displayed without error. In the current implementation, it is assumed that both page and pageSize are non-negative integers. Unfortunately, due to Elasticsearch currently not being implemented, the third parameter 'query' has not been given functionality. 
+
+http://127.0.0.1:5000/contact/{name}
+This serves POST, GET, PUT and DELETE requests. The GET request handler returns the contact's data if {name} corresponds to a valid entry, otherwise returns an error message. The DELETE request handler likewise deletes the contact's data if {name} corresponds to a valid entry, otherwise returns an error message.
+
+TESTING:
+This API's functionality was tested with Insomnia (https://insomnia.rest/). A full list of requests used to test the API (copied as curl commands) can be found in the testing.txt file. 
--- adbook.py
+++ adbook.py
@@ -1,20 +1,87 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from elasticsearch import Elasticsearch
 
+# creating flask instance
 app = Flask(__name__)
-api = Api(app)
 
-class Contact(Resource):
+# Hard-coded list which acts as dummy data for the contacts list.
+# In the full application, data would be sent to and retrieved from
+# an Elasticsearch data store. Here, the 'name' field is used as a
+# unique identifier for each data entry.
+contacts = [
+    {'name': 'Anna', 'address': '15 Princeton Street', 'contact': '9852207700'}, 
+    {'name': 'Barry', 'address': '6 Tower Road', 'contact': '2269004567'},
+    {'name': 'Cheryl', 'address': 'Ashton House', 'contact': '8287590001'}, 
+    {'name': 'David', 'address': '12 King Complex', 'contact': '3340901243'}
+]
 
-	def post(self):
+# Endpoint for the home page (i.e. http://127.0.0.1:5000).
+# Not required, but it seems like good practice to have it.
+@app.route('/', methods=['GET'])
+def homepage():
+    return "Coding challenge - Address Book"
 
-	def get(self, name):
+# Endpoint for GET /contact?pageSize={}&page={}&query={} requests.
+# Unfortunately, query={} is unused due to Elasticsearch not being
+# integrated.
+@app.route('/contact', methods=['GET'])
+def contact_all():
+    # Checks if query parameters are present.
+    if 'pageSize' in request.args and 'page' in request.args:
+        
+        pageSize = int(request.args['pageSize'])
+        page = int(request.args['page'])
+        
+        # The subsection of data to be shown for this particular 
+        # pageSize and page begins at 'start' and stops at 'end'.
+        # This implementation assumes that both pageSize and page
+        # are non-negative integers and that page numbers start 
+        # from 0. 
+        
+        start = pageSize * page
+        # If 'start' is beyond the end of the list, returns
+        # an error message.
+        if(start >= len(contacts)):
+            return 'Error: pageSize/page exceeds length of data'
+        
+        end = start + pageSize
+        # If 'start' is within list, but 'end' is outside it,
+        # 'end' is truncated to stop at the last entry.
+        if(end > len(contacts)):
+            end = len(contacts)
+        
+        # Returns the appropriate sublist from start to end.
+        return jsonify(contacts[start:end])
+    
+    # Returns error if query parameters not present.    
+    else:
+        return 'Error: pageSize or page fields not provided'
 
-	def put(self, name):
-
-	def delete(self, name):
-
-api.add_resource(Contact, "/contact/<string:name>")
-
-if __name__ == "__main__":
-	app.run()
+# Endpoint for POST/GET/PUT/DELETE /contact/{name} requests.
+@app.route('/contact/<name>', methods=['GET', 'DELETE'])
+def contact_id(name):
+    # Subsection for GET requests.
+    if request.method == 'GET':
+        # Searches the list of contacts using the unique 'name' 
+        # identifier. If the desired entry is found, it is returned.
+        # Else, an error message is displayed.  
+        for contact in contacts:
+            if(contact['name'] == name):
+                return jsonify(contact['name'])
+        return 'Error: Contact not found'        
+    
+    # Subsection for DELETE requests.
+    if request.method == 'DELETE':
+        # Searches the list of contacts using the unique 'name' 
+        # identifier. If the desired entry is found, it is deleted
+        # and a confirmation message is returned. Else, an error 
+        # message is displayed.
+        for contact in contacts:
+            if(contact['name'] == name):
+                contacts[:] = [c for c in contacts if c['name'] != name]
+                return 'contact successfully deleted'
+        return 'Error: Contact not found'
+    
+    # Returns error if an invalid method is used.
+    else:
+        return 'Error: Invalid request method'
