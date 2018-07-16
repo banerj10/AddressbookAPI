3459071184a6e31aa5233d9dfc5e2136db9da41b
Anirban <banerj10@illinois.edu>
2018-07-16 15:50:10 +0700

    saving updates so far

--- adbook.py
+++ adbook.py
@@ -1,5 +1,20 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
+app = Flask(__name__)
+api = Api(app)
+
+class Contact(Resource):
+
+	def post(self):
+
+	def get(self, name):
+
+	def put(self, name):
+
+	def delete(self, name):
+
+api.add_resource(Contact, "/contact/<string:name>")
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
