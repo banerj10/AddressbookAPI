3889e84cf935b303ab0758c4c1f79e6b473e3c3a
Anirban <banerj10@illinois.edu>
2018-07-17 13:14:41 +0700

    functionality, readme and comments, testing

--- README.txt
+++ README.txt
@@ -1,10 +1,10 @@
 RESTful API for an address book, implemented in Python using Flask.
-
 A list of dependencies can be found in the requirements.txt file.
+The flask app can be run from the command line using 'set FLASK_APP=hello.py' and 'flask run'. (In Linux systems, 'export' should be used in place of 'set'.) 
 
 DATA:
 
-The contact data in the address book as three fields: name, address, and phone number. Of these three, name is used as a unique identifier (i.e. no two entries have the same name).
+The contact data in the address book as three fields: name, address, and phone number. Of these three, name is used as a unique identifier (i.e. no two entries have the same name). When adding or updating entries, three checks are performed: that the name have no digits, that the phone number have not alphabets, and that the phone number consist of exactly nine characters.
 Currently, the contact data is sourced from the hard-coded 'contacts' list in adbook.py. In the full implementation, the contact data would be stored on an Elasticsearch data store instead, and all references to the list would be replaced with Elasticsearch calls. 
 
 ENDPOINTS:
@@ -13,10 +13,20 @@ http://127.0.0.1:5000
 This serves as a home page. In the current implementation, it does not have any significant functionality and merely provides a title page.
 
 http://127.0.0.1:5000/contact?pageSize={}&page={}&query={}
-This serves GET requests. It provides a listing of the contact data. THe user selects how many entries are to be shown with pageSize, and chooses the offset using page (starting at 0). If the pageSize/page selection is completely outside the list bounds, an error message is returned. However, if it is only partially outsie the list bounds, all entries until the end of the list are displayed without error. In the current implementation, it is assumed that both page and pageSize are non-negative integers. Unfortunately, due to Elasticsearch currently not being implemented, the third parameter 'query' has not been given functionality. 
+This serves GET requests. It provides a listing of the contact data. THe user selects how many entries are to be shown with pageSize, and chooses the offset using page (starting at 0). If the pageSize/page selection is completely outside the list bounds, an error message is returned. However, if it is only partially outsie the list bounds, all entries until the end of the list are displayed without error. In the current implementation, it is assumed that both page and pageSize are non-negative integers. Unfortunately, due to Elasticsearch currently not being implemented, the third parameter 'query' has not been given functionality.
+
+http://127.0.0.1:5000/contact
+This serves POST requests. It requires three parameters in the request body: name, address and phone (the same as the fields in the contact data). If the paramters are valid (see the DATA section) and the name provided does not already exist in the contact data, the request data is added as a new contact.
 
 http://127.0.0.1:5000/contact/{name}
-This serves POST, GET, PUT and DELETE requests. The GET request handler returns the contact's data if {name} corresponds to a valid entry, otherwise returns an error message. The DELETE request handler likewise deletes the contact's data if {name} corresponds to a valid entry, otherwise returns an error message.
+This serves GET, PUT and DELETE requests. The GET request handler returns the contact's data if {name} corresponds to a valid entry, otherwise returns an error message. The PUT handler,  requires two parameters in the reuqest body: address and phone. (We do not require the name here as {name} is already present). If the paramters are valid (see the DATA section) and the name field corresponds to a valid entry in the contact data, the entry is updated with the new phone number and address. The DELETE request handler deletes the contact's data if {name} corresponds to a valid entry, otherwise returns an error message.
 
 TESTING:
-This API's functionality was tested with Insomnia (https://insomnia.rest/). A full list of requests used to test the API (copied as curl commands) can be found in the testing.txt file. 
+
+This API's functionality was tested with Insomnia (https://insomnia.rest/), an open-source software for sending HTTP requests. A list of requests used to test the API (copied as curl commands) can be found in the testing.txt file.
+
+REFERENCES:
+https://auth0.com/blog/developing-restful-apis-with-python-and-flask/
+https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
+https://stackoverflow.com/questions/22947905/flask-example-with-post
+https://stackoverflow.com/questions/4526273/what-does-enctype-multipart-form-data-mean
--- adbook.py
+++ adbook.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 # an Elasticsearch data store. Here, the 'name' field is used as a
 # unique identifier for each data entry.
 contacts = [
-    {'name': 'Anna', 'address': '15 Princeton Street', 'contact': '9852207700'}, 
-    {'name': 'Barry', 'address': '6 Tower Road', 'contact': '2269004567'},
-    {'name': 'Cheryl', 'address': 'Ashton House', 'contact': '8287590001'}, 
-    {'name': 'David', 'address': '12 King Complex', 'contact': '3340901243'}
+    {'name': 'Anna', 'address': '15 Princeton Street', 'phone': '9852207700'}, 
+    {'name': 'Barry', 'address': '6 Tower Road', 'phone': '2269004567'},
+    {'name': 'Cheryl', 'address': 'Ashton House', 'phone': '8287590001'}, 
+    {'name': 'David', 'address': '12 King Complex', 'phone': '3340901243'}
 ]
 
 # Endpoint for the home page (i.e. http://127.0.0.1:5000).
@@ -21,44 +21,73 @@ contacts = [
 def homepage():
     return "Coding challenge - Address Book"
 
-# Endpoint for GET /contact?pageSize={}&page={}&query={} requests.
-# Unfortunately, query={} is unused due to Elasticsearch not being
-# integrated.
-@app.route('/contact', methods=['GET'])
+# Endpoint for GET /contact?pageSize={}&page={}&query={} and
+# POST /contact requests. The GET request has all its parameters 
+# present in the URL and the POST request has parameters name,
+# address and contact present in the body in x-www-form-urlencoded
+# format. Unfortunately, for GET requests, query={} is unused due 
+# to Elasticsearch not being integrated.
+@app.route('/contact', methods=['GET', 'POST'])
 def contact_all():
-    # Checks if query parameters are present.
-    if 'pageSize' in request.args and 'page' in request.args:
-        
-        pageSize = int(request.args['pageSize'])
-        page = int(request.args['page'])
-        
-        # The subsection of data to be shown for this particular 
-        # pageSize and page begins at 'start' and stops at 'end'.
-        # This implementation assumes that both pageSize and page
-        # are non-negative integers and that page numbers start 
-        # from 0. 
-        
-        start = pageSize * page
-        # If 'start' is beyond the end of the list, returns
-        # an error message.
-        if(start >= len(contacts)):
-            return 'Error: pageSize/page exceeds length of data'
-        
-        end = start + pageSize
-        # If 'start' is within list, but 'end' is outside it,
-        # 'end' is truncated to stop at the last entry.
-        if(end > len(contacts)):
-            end = len(contacts)
-        
-        # Returns the appropriate sublist from start to end.
-        return jsonify(contacts[start:end])
-    
-    # Returns error if query parameters not present.    
-    else:
-        return 'Error: pageSize or page fields not provided'
+    # Subsection for GET requests.
+    if request.method == 'GET':
+        # Checks if query parameters are present in URL.
+        if ('pageSize' in request.args and 'page' in request.args):        
+            pageSize = int(request.args['pageSize'])
+            page = int(request.args['page'])        
+            # The subsection of data to be shown for this particular 
+            # pageSize and page begins at 'start' and stops at 'end'.
+            # This implementation assumes that both pageSize and page
+            # are non-negative integers and that page numbers start 
+            # from 0.        
+            start = pageSize * page
+            # If 'start' is beyond the end of the list, returns
+            # an error message.
+            if(start >= len(contacts)):
+                return 'Error: pageSize/page exceeds length of data'       
+            end = start + pageSize
+            # If 'start' is within list, but 'end' is outside it,
+            # 'end' is truncated to stop at the last entry.
+            if(end > len(contacts)):
+                end = len(contacts)        
+            # Returns the appropriate sublist from start to end.
+            return jsonify(contacts[start:end])    
+        # Returns error if query parameters not present.    
+        else:
+            return 'Error: parameters not provided in request URL'
 
-# Endpoint for POST/GET/PUT/DELETE /contact/{name} requests.
-@app.route('/contact/<name>', methods=['GET', 'DELETE'])
+    # Subsection for POST requests.
+    if request.method == 'POST':
+        # Checks if query parameters are present in request body.
+        if ('name' in request.form and 'address' in request.form and
+            'phone' in request.form):        
+            name = str(request.form['name'])
+            address = str(request.form['address'])
+            phone = str(request.form['phone'])
+            # Returns error message if name has digits or if
+            # phone number has letters or if phone number does
+            # not have 10 digits.
+            if(not name.isalpha() or not phone.isdigit() or
+                len(phone) != 10):
+                return 'Error: invalid values in parameters'
+            # Returns error if contact already exists.    
+            for contact in contacts:
+                if(contact['name'] == name):
+                    return 'Error: contact already present'
+            # If parameters are valid and contact does not already
+            # exist, appends contact and returns a success message. 
+            contacts.append({'name': name, 'address': address, 
+                'phone':phone})
+            return 'Contact successfully added' 
+        # Returns error if query parameters not present.    
+        else:
+            return 'Error: parameters not provided in request body'
+
+# Endpoint for GET/PUT/DELETE /contact/{name} requests.
+# These requests can be used respectively to display, update or 
+# delete a particular contact denoted by <name>, and return an
+# error mesage if <name> is not found.
+@app.route('/contact/<name>', methods=['GET', 'PUT', 'DELETE'])
 def contact_id(name):
     # Subsection for GET requests.
     if request.method == 'GET':
@@ -67,8 +96,31 @@ def contact_id(name):
         # Else, an error message is displayed.  
         for contact in contacts:
             if(contact['name'] == name):
-                return jsonify(contact['name'])
-        return 'Error: Contact not found'        
+                return jsonify(contact)
+        return 'Error: Contact not found'
+
+    # Subsection for PUT requests.
+    if request.method == 'PUT':
+        # Checks if query parameters are present in request body.
+        if ('address' in request.form and 'phone' in request.form):
+            address = str(request.form['address'])
+            phone = str(request.form['phone'])
+            # Returns error message if phone number has letters or
+            # if phone number does not have 10 digits.
+            if(not phone.isdigit() or len(phone) != 10):
+                return 'Error: invalid values in parameters'
+            # Checks if a contact with <name> exists. If yes, updates 
+            # data and returns a success message.
+            for contact in contacts:
+                if(contact['name'] == name):
+                    contact['address'] = address
+                    contact['phone'] = phone
+                    return 'Contact successfully updated'
+            # Returns error message if <name> is not found. 
+            return 'Error: Contact not found' 
+        # Returns error if query parameters not present.    
+        else:
+            return 'Error: parameters not provided in request body'        
     
     # Subsection for DELETE requests.
     if request.method == 'DELETE':
